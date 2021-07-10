da59ddb90631d77c436966121495112d99981f6e
teregudi <teregudi.tamashi@gmail.com>
2021-07-10 23:33:42 +0200

    platforms moving

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
     const doodler = document.createElement('div');
     let doodlerLeftSpace = 50;
-    let doodlerBottomSpace = 150;
+    let doodlerBottomSpace = 250;
     let isGameOver = false;
     let platformCount = 5;
+    let platforms = [];
 
     function createDoodler(){
         grid.appendChild(doodler);
@@ -32,6 +33,18 @@ document.addEventListener('DOMContentLoaded', () => {
             let platGap = 600 / platformCount;
             let newPlatBottom = 100 + i * platGap;
             let newPlatform = new Platform(newPlatBottom);
+            platforms.push(newPlatform);
+            console.log(platforms);
+        }
+    }
+
+    function movePlatforms(){
+        if (doodlerBottomSpace > 200){
+            platforms.forEach(platform => {
+                platform.bottom -= 4;
+                let visual = platform.visual;
+                visual.style.bottom = platform.bottom + 'px';
+            })
         }
     }
 
@@ -39,6 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!isGameOver){
             createDoodler();
             createPlatforms();
+            setInterval(movePlatforms, 30);
         }
     }
 

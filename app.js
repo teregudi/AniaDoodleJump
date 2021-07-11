12ffcc358178cdb72ae7694adf1e651fc24466c2
teregudi <teregudi.tamashi@gmail.com>
2021-07-11 18:34:39 +0200

    going left and right

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid');
     const doodler = document.createElement('div');
     let doodlerLeftSpace;
-    let doodlerBottomSpace = 250;
+    let startPoint = 250;
+    let doodlerBottomSpace = startPoint;;
     let isGameOver = false;
     let platformCount = 5;
     let platforms = [];
     let upTimerId;
     let downTimerId;
     let isJumping = true;
+    let isGoingLeft = false;
+    let isGoingRight = false;
+    let leftTimerId;
+    let rightTimerId;
 
     function createDoodler(){
         grid.appendChild(doodler);
@@ -58,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
         upTimerId = setInterval(function(){
             doodlerBottomSpace += 20;
             doodler.style.bottom = doodlerBottomSpace + 'px';
-            if (doodlerBottomSpace > 350){
+            if (doodlerBottomSpace > startPoint+200){
                 fall();
             }
         }, 30);
@@ -80,6 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     doodlerLeftSpace <= (platform.left + 85) &&
                     !isJumping){
                         console.log('landed');
+                        startPoint = doodlerBottomSpace;
                         jump();
                 }
             })
@@ -95,12 +101,51 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function control(e){
         if (e.key === 'ArrowLeft'){
-            //move left
+            moveLeft();
         } else if (e.key === 'ArrowRight'){
-            // move right
+            moveRight();
         } else if (e.key === 'ArrowUp'){
-            //moveStraight
+            moveStraight();
         }
+    }
+
+    function moveLeft(){
+        if (isGoingRight){
+            clearInterval(rightTimerId);
+            isGoingRight = false;
+        }
+        isGoingLeft = true;
+        leftTimerId = setInterval(function(){
+            if (doodlerLeftSpace >= 0){
+                doodlerLeftSpace -= 5;
+                doodler.style.left = doodlerLeftSpace + 'px';
+            } else {
+                moveRight();
+            }
+        }, 30);
+    }
+
+    function moveRight(){
+        if (isGoingLeft){
+            clearInterval(leftTimerId);
+            isGoingLeft = false;
+        }
+        isGoingRight = true;
+        rightTimerId = setInterval(function(){
+            if (doodlerLeftSpace <= 340){
+                doodlerLeftSpace += 5;
+                doodler.style.left = doodlerLeftSpace + 'px';
+            } else {
+                moveLeft();
+            }
+        }, 30);
+    }
+
+    function moveStraight(){
+        isGoingLeft = false;
+        isGoingRight = false;
+        clearInterval(leftTimerId);
+        clearInterval(rightTimerId);
     }
 
     function start(){
@@ -109,6 +154,7 @@ document.addEventListener('DOMContentLoaded', () => {
             createDoodler();
             setInterval(movePlatforms, 30);
             jump();
+            document.addEventListener('keyup', control);
         }
     }
 
